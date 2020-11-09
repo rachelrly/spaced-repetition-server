bd563157389ea60d8a8eca056b8a959d531994e0
Rachel Reilly <r.a.reill18@gmail.com>
2020-11-09 09:21:11 -0600

    changed db seed file

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -9,26 +9,30 @@ INSERT INTO "user" ("id", "username", "name", "password")
 VALUES
   (
     1,
-    'admin',
-    'Dunder Mifflin Admin',
+    'horatia241',
+    'Horatia de Rome',
     -- password = "pass"
     '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
   );
 
 INSERT INTO "language" ("id", "name", "user_id")
 VALUES
-  (1, 'French', 1);
+  (1, 'Latin', 1);
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'ante', 'Before', 2),
+  (2, 1, 'contra', 'Opposite, against, contrary to', 3),
+  (3, 1, 'quia', 'Because', 4),
+  (4, 1, 'Solus', 'Alone, only, lonely, single', 5),
+  (5, 1, 'Totus', 'All, whole, entire', 6),
+  (6, 1, 'Dicere', 'To say', 7),
+  (7, 1, 'Videre', 'To look, see', 8),
+  (8, 1, 'Facere', 'To make, do', 9),
+  (9, 1, 'Totus', 'All, whole, entire', 10),
+  (10, 1, 'Dies', 'Day', 11),
+  (11, 1, 'Nomen', 'Name', 12),
+  (12, 1, 'Cibus', 'Food', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
